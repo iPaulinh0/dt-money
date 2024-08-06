08034dc9f3475a9a77667bdeceefa5daddd30cdf
“iPaulinh0” <pauloaraujo2607@gmail.com>
2024-08-06 13:44:13 -0300

    setup: configuration of transaction table history

--- src/pages/Trasactions/index.tsx
+++ src/pages/Trasactions/index.tsx
@@ -1,11 +1,35 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return(
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.500,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Manutenção na embreagem</td>
+              <td>
+                <PriceHighlight variant="outcome">R$ -2.500,00</PriceHighlight>
+              </td>
+              <td>Carro</td>
+              <td>16/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
