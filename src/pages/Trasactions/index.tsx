15efc6d4716b78871403d07264dff2370fbf6f7d
“iPaulinh0” <pauloaraujo2607@gmail.com>
2024-08-16 05:05:55 -0300

    feature: create context provider and configuring components with useContext

--- src/pages/Trasactions/index.tsx
+++ src/pages/Trasactions/index.tsx
@@ -1,32 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
-
-interface Transactions {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string
-}
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Transactions() {
-
-  const [transactions, setTransactions] = useState<Transactions[]>([])
-
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3000/transactions')
-    const data = await response.json()
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, [])
-
+  const {transactions} = useContext(TransactionsContext)
   return(
     <div>
       <Header />
